aba25836ba083abf8d5c0fd485c42460d1419c59
Manmath Hatte <manmathcode@gmail.com>
2026-05-29 11:19:30 +0530

    Link to this code: https://cses.fi/paste/8783c0106ba202aa108a0fe/

---
+++ String Algorithms/Finding_Borders.cpp
@@ -0,0 +1,68 @@
+/**
+ * author: Manmath_H
+ * created: 29.05.2026 10:05:45
+ * Link to this code: https://cses.fi/paste/8783c0106ba202aa108a0fe/
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int,int>
+#define pb push_back
+typedef __int128 Int;
+#define endl '\n'
+#define INF (int)1e18
+#define mod 998244353
+using u64 = unsigned long long;
+
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+vector<int> PI(const string &s){
+    int n = s.size();
+
+    vector<int> pi(n);
+    for(int i = 1, j = 0; i<n; i++){
+        while(j > 0 && s[i]!= s[j])     j = pi[j-1];
+
+        if(s[i] == s[j])    j++;
+        pi[i] = j;
+    }
+    //for(int &i : pi)    cerr << i << " ";
+    return pi;
+}
+
+void solve() {
+    string s;
+    cin >> s;
+    int n = s.size();
+    vector<int> pi = PI(s);
+    vector<int> ans;
+    int j = n-1;
+    
+    while(j > 0 && pi[j] > 0){
+        ans.push_back(pi[j]);
+        j = pi[j]-1;
+    }
+
+    reverse(ans.begin(), ans.end());
+
+    for(int &i : ans)   cout << i << " ";
+}
+
+int32_t main() {
+  auto begin = std::chrono::high_resolution_clock::now();
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int t = 1;
+  //cin >> t;
+  while(t--){
+    solve();
+  }
+
+  auto end = std::chrono::high_resolution_clock::now();
+  auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+  //cerr << "Code_by_Manmath_H: " << elapsed.count() * 1e-9 << " sec.\n";
+  return 0;
+}
