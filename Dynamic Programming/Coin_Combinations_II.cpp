cd9e8c9af30afbf5d35377d0b8a803437ae15900
Manmath Hatte <manmathcode@gmail.com>
2026-06-15 22:59:06 +0530

    Link to this code: https://cses.fi/paste/09cfcba1dd00a348bfc3c9/

---
+++ Dynamic Programming/Coin_Combinations_II.cpp
@@ -0,0 +1,57 @@
+//Link to this code: https://cses.fi/paste/09cfcba1dd00a348bfc3c9/
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ld long double
+#define ll long long
+#define f(i, m, n) for (int i = m; i <= n; i++)
+#define rf(i, m, n) for (int i = m; i >= n; i--)
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repr(i, n) for (int i = (n)-1; i >= 0; --i)
+#define mod 1000000007
+#define MOD 998244353
+constexpr ll INF = 4e18;
+ 
+using vi = vector<int>;
+using vll = vector<ll>;
+ 
+///////////////////////////////////////////////////////////////////////////////////////////
+ 
+ 
+void solve() {
+    ll n, target;
+    cin >> n >> target;
+    
+    vll coins(n);
+    for(ll i = 0; i < n; i++)   cin >> coins[i];
+ 
+    vll dp(target + 1, 0);
+    dp[0] = 1;
+ 
+    for(ll i = 0; i < n; i++) {
+        for(ll j = 1; j <= target; j++) {
+            if( j >= coins[i]){
+                dp[j] = ( dp[j] + dp[j-coins[i]]) % mod;
+            }
+        }
+        //cout << dp[i] << " ";
+    }
+ 
+    cout << dp[target];
+    
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    // ll t;
+    // cin >> t;
+ 
+   solve();
+ 
+    // while (t--) {
+    //     solve();
+    // }
+ 
+    return 0;
+}
